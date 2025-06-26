ac75dbcdb3891b1fed7c7e312bc4cbb744db7531
cookeyholder <cookeyholder@gmail.com>
2025-06-27 05:36:39 +0800

    refactor: 優化「篩選排入考程補考名單」的處理邏輯與各班級任課教師提取方式

--- dataFilter.js
+++ dataFilter.js
@@ -1,4 +1,7 @@
 function getFilteredData() {
+    // 清除「排入考程的補考名單」工作表的內容
+    initialize();
+
     // 取得「註冊組補考名單」的欄位索引
     const [unfilteredSheetHeaders, ...unfilteredData] = unfilteredSheet
         .getDataRange()
@@ -28,69 +31,77 @@ function getFilteredData() {
     const isManualGradedColumn = candidateSheetHeaders.indexOf("人工");
 
     let candidateSubjects = {};
-    candidateSheetData.forEach(function (row) {
-        if (row[makeUpColumn] == true) {
-            candidateSubjects[row[filteredSubjectCodeColumn]] = {
+    for (const row of candidateSheetData) {
+        const needsMakeUpExam = row[makeUpColumn] === true;
+
+        if (needsMakeUpExam) {
+            const subjectCode = row[filteredSubjectCodeColumn];
+            candidateSubjects[subjectCode] = {
                 isComputerGraded: row[isComputerGradedColumn],
                 isManualGraded: row[isManualGradedColumn],
             };
         }
-    });
+    }
 
-    let openTeacherName = {};
-    openData.forEach(function (row) {
-        if (row[teacherNameColumn].toString().length > 10) {
+    // 建立班級科目與任課教師的對應表
+    const createClassSubjectKey = (className, subjectName) =>
+        className.toString() + subjectName.toString();
+
+    const extractTeacherName = (teacherNameText) => {
+        const teacherName = teacherNameText.toString();
+        return teacherName.length > 10
+            ? teacherName.split(",")[0].slice(7)
+            : teacherName.slice(6);
+    };
+
+    const openTeacherName = Object.fromEntries(
+        openData.map((row) => [
+            createClassSubjectKey(
+                row[openClassNameColumn],
+                row[openSubjectNameColumn]
+            ),
+            extractTeacherName(row[teacherNameColumn]),
+        ])
+    );
+
+    const createStudentRecord = (row) => {
+        const subjectCode = row[subjectCodeColumn];
+        const subjectInfo = candidateSubjects[subjectCode];
+
+        return [
+            getDepartmentName(row[classNameColumn]), // 科別
+            getGrade(row[classNameColumn]), // 年級
+            getClassCode(row[classNameColumn]), // 班級代碼
+            row[classNameColumn], // 班級
+            row[seatNumberColumn], // 座號
+            row[studentIdColumn], // 學號
+            row[studentNameColumn], // 姓名
+            row[subjectNameColumn], // 科目名稱
+            0, // 節次預設為0
+            0, // 試場預設為0
+            "", // 小袋序號
+            "", // 小袋人數
+            "", // 大袋序號
+            "", // 大袋人數
+            "", // 班級人數
+            "", // 時間
+            subjectInfo.isComputerGraded ? "☑" : "☐", // 電腦
+            subjectInfo.isManualGraded ? "☑" : "☐", // 人工
             openTeacherName[
-                row[openClassNameColumn].toString() +
-                    row[openSubjectNameColumn].toString()
-            ] = row[teacherNameColumn].toString().split(",")[0].slice(7);
-        } else {
-            openTeacherName[
-                row[openClassNameColumn].toString() +
-                    row[openSubjectNameColumn].toString()
-            ] = row[teacherNameColumn].toString().slice(6);
-        }
-    });
+                createClassSubjectKey(
+                    row[classNameColumn],
+                    row[subjectNameColumn]
+                )
+            ], // 任課老師
+        ];
+    };
 
-    // 清除「排入考程的補考名單」內容
-    initialize();
+    const isEligibleForMakeUp = (row) =>
+        candidateSubjects.hasOwnProperty(row[subjectCodeColumn]);
 
-    let nameList = [];
-    unfilteredData.forEach(function (row) {
-        if (Object.keys(candidateSubjects).includes(row[subjectCodeColumn])) {
-            // 科別	年級	班級代碼	班級	座號	學號	姓名	科目名稱	節次	試場	小袋序號	小袋人數	大袋序號	大袋人數	班級人數	時間	電腦	人工	任課老師
-            let tmp = [
-                getDepartmentName(row[classNameColumn]), // 科別
-                getGrade(row[classNameColumn]), // 年級
-                getClassCode(row[classNameColumn]), // 班級代碼
-                row[classNameColumn], // 班級
-                row[seatNumberColumn], // 座號
-                row[studentIdColumn], // 學號
-                row[studentNameColumn], // 姓名
-                row[subjectNameColumn], // 科目名稱
-                (row[8] = 0), // 節次預設為0
-                (row[9] = 0), // 試場預設為0
-                "", // 小袋序號
-                "", // 小袋人數
-                "", // 大袋序號
-                "", // 大袋人數
-                "", // 班級人數
-                "", // 時間
-                candidateSubjects[row[subjectCodeColumn]]["isComputerGraded"]
-                    ? "☑"
-                    : "☐", // 電腦
-                candidateSubjects[row[subjectCodeColumn]]["isManualGraded"]
-                    ? "☑"
-                    : "☐", //人工
-                openTeacherName[
-                    row[classNameColumn].toString() +
-                        row[subjectNameColumn].toString()
-                ], // 任課老師
-            ];
-
-            nameList.push(tmp);
-        }
-    });
+    const nameList = unfilteredData
+        .filter(isEligibleForMakeUp)
+        .map(createStudentRecord);
 
     filteredSheet
         .getRange(2, 1, nameList.length, nameList[0].length)
